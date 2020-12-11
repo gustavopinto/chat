b869f0741797474a41f02f7e2b04da9f3a186f46
JoseCarlosNF <josecarlos.n.f99@gmail.com>
2020-12-11 09:41:50 -0300

    ✨ adicionarParticipante e removerParticipante
    Os métodos foram adicionados, tendo como base o código dos métodos de mesmo nome da classe Grupo

--- src/prova1b/Moderador.java
+++ src/prova1b/Moderador.java
@@ -1,8 +1,14 @@
 package prova1b;
 
+import static java.util.Arrays.copyOf;
+
 public class Moderador extends Participante implements SuperParticipante {
 	private String nome;
 	private Grupo grupo;
+
+	public Moderador(String nome) {
+		this.nome = nome;
+	}
 
 	public Grupo criarGrupo(String nome){
 		Grupo g = new Grupo(nome);
@@ -13,11 +19,24 @@ public class Moderador extends Participante implements SuperParticipante {
 
 	@Override
 	public void adicionarParticipante(Participante p) {
-
+		Participante[] ps = copyOf(grupo.getParticipantes(), grupo.getParticipantes().length + 1);
+		ps[grupo.getParticipantes().length] = p;
+		this.grupo.participantes = ps;
 	}
 
 	@Override
 	public boolean removerParticipante(Participante p) {
+		for (int i = 0; i < grupo.getParticipantes().length; i++) {
+			if(grupo.getParticipantes()[i].equals(p)) {
+				Participante[] ps = new Participante[grupo.getParticipantes().length -1];
+				int remainingElements = grupo.getParticipantes().length - (i + 1);
+				System.arraycopy(grupo.getParticipantes(), 0, ps, 0, i);
+				System.arraycopy(grupo.getParticipantes(), i+ 1, ps, i, remainingElements);
+
+				this.grupo.participantes = ps;
+				return true;
+			}
+		}
 		return false;
 	}
 

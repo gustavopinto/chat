3677a9fd2722f3f43eabe8937deeddca2046e0bc
JoseCarlosNF <josecarlos.n.f99@gmail.com>
2020-12-11 11:48:36 -0300

    🐛 Correção na definição do construtor

--- src/prova1b/Participante.java
+++ src/prova1b/Participante.java
@@ -3,6 +3,10 @@ package prova1b;
 public abstract class Participante {
 
 	private String nome;
+
+	public Participante(String nome){
+		this.nome = nome;
+	}
 	
 	abstract void enviarMensagem(String texto, Grupo g);
 

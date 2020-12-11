a985a2ac4aa0c54f26fd9f67132a1766b425c0e7
JoseCarlosNF <josecarlos.n.f99@gmail.com>
2020-12-11 09:07:42 -0300

    ♻️ Moderador implementa a interface SuperParticipante

--- src/prova1b/Moderador.java
+++ src/prova1b/Moderador.java
@@ -1,14 +1,28 @@
 package prova1b;
 
-import static java.util.Arrays.copyOf;
-
 public class Moderador extends Participante implements SuperParticipante {
 	private String nome;
+	private Grupo grupo;
 
 	public Grupo criarGrupo(String nome){
 		Grupo g = new Grupo(nome);
 		g.adicionarParticipantes(this);
 		this.grupo = g;
 		return g;
+	}
+
+	@Override
+	public void adicionarParticipante(Participante p) {
+
+	}
+
+	@Override
+	public boolean removerParticipante(Participante p) {
+		return false;
+	}
+
+	@Override
+	void enviarMensagem(String texto, Grupo g) {
+
 	}
 }
